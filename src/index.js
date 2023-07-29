24872dcf552e40e584e0603ae13deb1f06b595e2
Ethan Hann <ethan@snowshoestamp.com>
2023-07-29 15:21:54 -0400

    WIP update lp

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './css/styles.scss';
+
+document.getElementById('copyright-year').textContent = new Date().getFullYear().toString();
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig, splitVendorChunkPlugin} from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+
+export default defineConfig({
+    build: {
+        outDir: '../dist/',
+        manifest: true,
+        minify: true,
+        sourcemap: true,
+        emptyOutDir: true,
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
+    plugins: [
+        splitVendorChunkPlugin(),
+        legacy({
+            targets: ['defaults'],
+        }),
+    ],
+});
